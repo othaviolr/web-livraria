11e4a157e60adb7c06e8c36e5ec8f5554685e9dd
othaviolr <othavionogueira2003@gmail.com>
2025-05-27 18:48:37 -0300

    fixing: ranking entity

--- WebApiLivraria.Application/Dto/LivroDto.cs
+++ WebApiLivraria.Application/Dto/LivroDto.cs
@@ -6,6 +6,9 @@
         public string Titulo { get; set; }
         public int AutorId { get; set; }
         public int EditoraId { get; set; }
+
+        public DateTime AnoPublicacao { get; set; }
+
         public List<int> Generos { get; set; } = new();
     }
 }
