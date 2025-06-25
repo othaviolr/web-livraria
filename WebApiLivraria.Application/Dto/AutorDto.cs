12e6ea7fceef098740caabf90c43ed763cc5eb81
othaviolr <othavionogueira2003@gmail.com>
2025-06-25 18:48:47 -0300

    add: author registration more options

--- WebApiLivraria.Application/Dto/AutorDto.cs
+++ WebApiLivraria.Application/Dto/AutorDto.cs
@@ -4,5 +4,9 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public string? Biografia { get; set; }
+        public DateTime? DataNascimento { get; set; }
+        public string? LocalNascimento { get; set; }
+        public string? FotoUrl { get; set; }
     }
 }
--- WebApiLivraria.Application/Services/AutorService.cs
+++ WebApiLivraria.Application/Services/AutorService.cs
@@ -2,6 +2,9 @@
 using WebApiLivraria.Application.Interfaces;
 using WebApiLivraria.Domain.Entities;
 using WebApiLivraria.Domain.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace WebApiLivraria.Application.Services
 {
@@ -17,35 +20,34 @@ namespace WebApiLivraria.Application.Services
         public async Task<IEnumerable<AutorDto>> ListarAsync()
         {
             var autores = await _autorRepository.ListarAsync();
-            return autores.Select(a => new AutorDto
-            {
-                Id = a.Id,
-                Nome = a.Nome
-            });
+            return autores.Select(a => MapToDto(a));
         }
 
         public async Task<AutorDto> ObterPorIdAsync(int id)
         {
             var autor = await _autorRepository.ObterPorIdAsync(id);
             if (autor == null) return null;
-
-            return new AutorDto
-            {
-                Id = autor.Id,
-                Nome = autor.Nome
-            };
+            return MapToDto(autor);
         }
 
         public async Task<AutorDto> AdicionarAsync(AutorDto dto)
         {
-            var autor = new Autor(dto.Nome);
+            var ultimoId = await _autorRepository.ObterMaiorIdAsync();
+            int novoId = ultimoId + 1;
+
+            var autor = new Autor(
+                dto.Nome,
+                dto.Biografia,
+                dto.DataNascimento,
+                dto.LocalNascimento,
+                dto.FotoUrl
+            );
+
+            autor.SetId(novoId);
+
             await _autorRepository.AdicionarAsync(autor);
 
-            return new AutorDto
-            {
-                Id = autor.Id,
-                Nome = autor.Nome
-            };
+            return MapToDto(autor);
         }
 
         public async Task AtualizarAsync(AutorDto dto)
@@ -53,13 +55,33 @@ namespace WebApiLivraria.Application.Services
             var autorExistente = await _autorRepository.ObterPorIdAsync(dto.Id);
             if (autorExistente == null) return;
 
-            autorExistente.AtualizarNome(dto.Nome);
+            autorExistente.Atualizar(
+                dto.Nome,
+                dto.Biografia,
+                dto.DataNascimento,
+                dto.LocalNascimento,
+                dto.FotoUrl
+            );
+
             await _autorRepository.AtualizarAsync(autorExistente);
         }
 
         public async Task RemoverAsync(int id)
         {
             await _autorRepository.RemoverAsync(id);
+        }
+
+        private AutorDto MapToDto(Autor autor)
+        {
+            return new AutorDto
+            {
+                Id = autor.Id,
+                Nome = autor.Nome,
+                Biografia = autor.Biografia,
+                DataNascimento = autor.DataNascimento,
+                LocalNascimento = autor.LocalNascimento,
+                FotoUrl = autor.FotoUrl
+            };
         }
     }
 }
