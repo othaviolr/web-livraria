78787203e0c319e3b9ae963169c663e687102d64
othaviolr <othavionogueira2003@gmail.com>
2025-06-29 14:04:04 -0300

    feat(user): add support for roles (Reader/Author) and user profile updates

---
+++ WebApiLivraria.Application/Dto/AtualizarPerfilRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiLivraria.Application.UseCases.Usuarios;
+
+public class AtualizarPerfilRequest
+{
+    public string NomeUsuario { get; set; } = null!;
+    public string? FotoUrl { get; set; }
+    public string? Cidade { get; set; }
+    public string Role { get; set; } = "Leitor";
+}
