a023bfafdeeffbb119479e13fc29e9fd8720f30e
othaviolr <othavionogueira2003@gmail.com>
2025-06-29 15:32:25 -0300

    feat(users); triggering endpoint to receive user photo

---
+++ WebApiLivraria.Application/Dto/UsuarioPerfilDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiLivraria.Application.UseCases.Usuarios
+{
+    public class UsuarioPerfilDto
+    {
+        public string NomeUsuario { get; set; } = null!;
+        public string? FotoUrl { get; set; }
+        public string? Cidade { get; set; }
+        public string Role { get; set; } = "Leitor";
+    }
+}
