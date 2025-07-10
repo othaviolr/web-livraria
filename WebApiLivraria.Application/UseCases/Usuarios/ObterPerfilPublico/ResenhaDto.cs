949c60164bf9e8bf4b1f6f16eb731e550e57fc19
othav <othav@OTHVIO>
2025-07-09 22:23:33 -0300

    chore(Users): adjusted user requests

--- WebApiLivraria.Application/UseCases/Usuarios/ObterPerfilPublico/ResenhaDto.cs
+++ WebApiLivraria.Application/UseCases/Usuarios/ObterPerfilPublico/ResenhaDto.cs
@@ -4,6 +4,8 @@
     {
         public int LivroId { get; set; }
         public string Titulo { get; set; } = null!;
+        public string Autor { get; set; } = null!;    
+        public string ImagemUrl { get; set; } = null!;
         public int Nota { get; set; }
         public string Comentario { get; set; } = null!;
         public DateTime Data { get; set; }
