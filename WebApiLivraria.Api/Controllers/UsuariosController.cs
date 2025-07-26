a8fc0e6b46d7a396e2460e3fac8e074ca39bcd3d
othav <othav@OTHVIO>
2025-07-26 20:49:33 -0300

    feat: controller users

--- WebApiLivraria.Api/Controllers/UsuariosController.cs
+++ WebApiLivraria.Api/Controllers/UsuariosController.cs
@@ -19,7 +19,7 @@ using WebApiLivraria.Application.UseCases.Usuarios;
 namespace WebApiLivraria.Api.Controllers
 {
     [ApiController]
-    [Route("usuarios")]
+    [Route("api/usuarios")]
     public class UsuariosController : ControllerBase
     {
         private readonly IUsuarioRepository _usuarioRepository;
